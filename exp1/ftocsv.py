1fcf1f52014d16ce6336d6090a1228794be17b6d
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-08-12 00:13:26 -0400

    ADD community analysis

--- exp1/ftocsv.py
+++ exp1/ftocsv.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import pyarrow.feather as feather
+import pyarrow.parquet as pq
 import sys
 
-df = feather.read_feather(sys.argv[1])
-df.to_csv(sys.argv[1] + '.csv')
+print(sys.argv[1])
+df = pq.read_table(open(sys.argv[1], 'rb'))
+df.to_pandas().to_csv(sys.argv[1] + '.csv')
 
